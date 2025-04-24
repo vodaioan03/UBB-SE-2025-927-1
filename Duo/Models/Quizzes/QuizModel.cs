8540a52098d3547d9b3f4f45080083cf2ddc83e8
Zsigmond Artur <Zsigmond Artur@DESKTOP-6PKIB4N>
2025-04-24 12:21:39 +0300

    fix: cleaned up the folder loginc

--- Duo/Models/Quizzes/QuizModel.cs
+++
@@ -1,8 +0,0 @@
-// for fetching a quiz from API
-public class QuizModel
-{
-    public int Id { get; set; }
-    public int? SectionId { get; set; }
-    public List<int> ExerciseIds { get; set; }
-    public DateTime ExpirationTime { get; set; }
-}
