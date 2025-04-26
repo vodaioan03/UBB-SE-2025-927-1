dc19ff8a5cb64b34b23540d2e3f00861008a995e
Dragoș Șerban <dragos4002@gmail.com>
2025-04-26 19:23:09 +0300

    Updated IRepository for quiz and exercise management

--- Duo.Api/Repositories/IRepository.cs
+++ Duo.Api/Repositories/IRepository.cs
@@ -5,8 +5,18 @@ namespace Duo.Api.Repsitories
 {
     public interface  IRepository
     {
+        // Exercises
         Task<List<Exercise>> GetExercisesFromDbAsync();
-         
+        Task<Exercise> GetExerciseByIdAsync(int id);
+        Task AddExerciseAsync(Exercise exercise);
+        Task UpdateExerciseAsync(Exercise exercise);
+        Task DeleteExerciseAsync(int id);
+
+        //Quizzes
         Task<List<BaseQuiz>> GetQuizzesFromDbAsync();
+        Task<BaseQuiz> GetQuizByIdAsync(int id);
+        Task AddQuizAsync(BaseQuiz quiz);
+        Task UpdateQuizAsync(BaseQuiz quiz);
+        Task DeleteQuizAsync(int id);
     }
 }
