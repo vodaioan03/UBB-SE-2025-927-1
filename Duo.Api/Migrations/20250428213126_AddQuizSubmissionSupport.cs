473ad92985d47405714d2bdc66b9d8c1f851b9c8
Zsigmond Artur <Zsigmond Artur@DESKTOP-6PKIB4N>
2025-04-29 00:54:30 +0300

    fix: updated the migrations

---
+++ Duo.Api/Migrations/20250428213126_AddQuizSubmissionSupport.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Duo.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuizSubmissionSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QuizSubmissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuizId = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizSubmissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnswerSubmissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionId = table.Column<int>(type: "int", nullable: false),
+                    SelectedOptionIndex = table.Column<int>(type: "int", nullable: false),
+                    IsCorrect = table.Column<bool>(type: "bit", nullable: false),
+                    QuizSubmissionEntityId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnswerSubmissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AnswerSubmissions_QuizSubmissions_QuizSubmissionEntityId",
+                        column: x => x.QuizSubmissionEntityId,
+                        principalTable: "QuizSubmissions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnswerSubmissions_QuizSubmissionEntityId",
+                table: "AnswerSubmissions",
+                column: "QuizSubmissionEntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnswerSubmissions");
+
+            migrationBuilder.DropTable(
+                name: "QuizSubmissions");
+        }
+    }
+}
